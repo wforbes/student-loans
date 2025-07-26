fb1acc38f9a45179bfb15fa7165d4030c0b109cc
Will Forbes <wforbes87@gmail.com>
2025-07-25 17:56:40 -0700

    rename 'session-api' to fetchers, change structure
    - small change to rename the session fetcher file to be less pedantic

--- src/services/session/queries.ts
+++ src/services/session/queries.ts
@@ -1,13 +1,13 @@
-import { sessionApi } from "./session-api";
+import { verifySession } from "@/services/session/fetchers";
 import { useQuery } from "@tanstack/react-query";
-import { queryKeys } from "../query-keys";
+import { queryKeys } from "@/services/query-keys";
 import { useUtilities } from "@/services/useUtilities";
 
 export const useSessionQuery = () => {
 	const { handleAuthLogout } = useUtilities();
 	const { data, isLoading, isFetching, error } = useQuery({
 		queryKey: queryKeys.session.verify(),
-		queryFn: () => sessionApi.verifySession(handleAuthLogout),
+		queryFn: () => verifySession(handleAuthLogout),
 	});
 
 	return { data, isLoading, isFetching, error };
