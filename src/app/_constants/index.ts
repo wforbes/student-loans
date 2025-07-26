bd19c261f59ecbb21eec9ef0c2e5812f8986ccfa
Will Forbes <wforbes87@gmail.com>
2025-07-26 08:39:50 -0700

    add missing constants from previous commit

--- src/app/_constants/index.ts
+++ src/app/_constants/index.ts
@@ -3,6 +3,10 @@ export const appTitle = "PayDown";
 
 export const MESSAGES = {
 	FORM_ERROR: "Invalid form data",
+	EMAIL_TAKEN: "Email already taken",
+	USERNAME_TAKEN: "Username already taken",
+	PASSWORD_MISMATCH: "Passwords do not match",
+	CREATE_SESSION_ERROR: "Failed to create session, please try again later.",
 	SIGN_OUT_SUCCESS: "Successfully signed out",
 	SIGN_OUT_ERROR: "Failed to sign out, please try again later.",
 	LOGIN_ERROR: "Invalid email or password"
